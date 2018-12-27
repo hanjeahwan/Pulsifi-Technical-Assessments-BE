94880a1442791db2521dd68417f4beadbfa3da01
hanjeahwan <hanjeahwan@gmail.com>
2018-12-27 15:10:00 +0800

    web push

--- src/job/job.module.ts
+++ src/job/job.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JobService } from './job.service';
 import { JobEntity } from './entity/job.entity';
@@ -7,6 +7,7 @@ import { JobController } from './job.controller';
 @Module({
     imports: [
     TypeOrmModule.forFeature([JobEntity]),
+    HttpModule
     ],
     providers: [JobService],
     controllers: [JobController],
--- src/job/job.service.ts
+++ src/job/job.service.ts
@@ -5,22 +5,36 @@ import { JobEntity } from './entity/job.entity';
 import { JobModel } from './model/job.model';
 import { CondationsInterface } from './interfaces/condition.interface';
 import { Pagination, PaginationOptionsInterface } from './../paginate';
+import axios, { AxiosInstance } from 'axios';
+import { defer, Observable } from 'rxjs';
 
 @Injectable()
 export class JobService {
+    private client: AxiosInstance;
+
     constructor(
         @InjectRepository(JobEntity)
         private readonly jobRepository: Repository<JobEntity>,
-    ) { }
+    ) {
+        this.client = axios.create({
+            baseURL: 'https://onesignal.com/api/v1/',
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Authorization": "Basic NzIzN2I4Y2MtZWI1NS00NGNjLWExODUtNjQyNzMyMzUwYzlj"
+            }
+        });
+    }
 
     async create(job: JobModel): Promise<JobEntity> {
-        return await this.jobRepository.save(
+        const data = await this.jobRepository.save(
             this.jobRepository.create(job)
         );
+        const push = await this.pushNotification(data);
+        return data
     }
 
     async update(job: JobModel): Promise<UpdateResult> {
-      return await this.jobRepository.update(job.id, job);
+        return await this.jobRepository.update(job.id, job);
     }
 
     async findAll(options: PaginationOptionsInterface): Promise<Pagination<JobEntity>> {
@@ -49,6 +63,21 @@ export class JobService {
     }
 
     async findById(id: number): Promise<JobEntity | null> {
-      return await this.jobRepository.findOne(id);
+        return await this.jobRepository.findOne(id);
+    }
+
+
+    async pushNotification(job: JobModel): Promise<any> {
+        const response = await this.client({
+            url: 'notifications',
+            method: 'post',
+            data: {
+                app_id: "cd715dec-a2c8-4850-9ad3-831e8485a066",
+                headings: { "en": job.title },
+                contents: { "en": job.breif },
+                included_segments: ["All"],
+            }
+        });
+        return response.data;
     }
 }
